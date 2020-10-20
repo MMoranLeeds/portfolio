f5665c87600c02a111166aedc87c1255181ae19a
Matthew Moran <matthewdm@live.co.uk>
2020-10-20 16:27:59 +0100

    revert featured proj link and bubble up css amend

--- src/components/FeaturedProject.vue
+++ src/components/FeaturedProject.vue
@@ -1,35 +1,18 @@
 <template>
   <section data-section-featured>
     <article>
-      <router-link
-        v-bind:aria-label="'View the ' + featured.title + 'project'"
-        v-bind:to="{
-          name: 'ProjectOverview',
-          params: { project: featured.reference },
-        }"
-        tabindex="-1"
-      >
-        <img
-          v-bind:src="'img/header-projects/' + featured.image + '.jpg'"
-          v-bind:alt="featured.altText"
-          loading="lazy"
-          v-if="!handleLoading"
-        />
-      </router-link>
+      <img
+        v-bind:src="'img/header-projects/' + featured.image + '.jpg'"
+        v-bind:alt="featured.altText"
+        loading="lazy"
+        v-if="!handleLoading"
+      />
 
       <div class="content">
-        <h3>{{ featured.title }}</h3>
+        <h3>{{featured.title}}</h3>
         <p v-html="featured.bodyContent"></p>
 
-        <router-link
-          class="button"
-          v-bind:aria-label="'View the ' + featured.title + 'project'"
-          v-bind:to="{
-            name: 'ProjectOverview',
-            params: { project: featured.reference },
-          }"
-          >View</router-link
-        >
+        <router-link class="button" v-bind:aria-label="'View the ' + featured.title + 'project'" v-bind:to="{name: 'ProjectOverview', params:{project:featured.reference}}">View</router-link>
       </div>
     </article>
   </section>
