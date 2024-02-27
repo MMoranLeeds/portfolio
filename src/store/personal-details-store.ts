b68493530a4068c5a12d76c3f69911bac91d6d2b
Matthew Moran <matthew.moran@fruugo.com>
2024-02-27 11:16:27 +0000

    intro update

--- src/store/personal-details-store.ts
+++ src/store/personal-details-store.ts
@@ -5,7 +5,7 @@ export const personalDetailsStore: Module<any, any> = {
     state: {
         name: "Matthew Moran",
         title: "Lead Product Designer and Front-end Developer",
-        introduction: "An experienced Lead Product Designer and Front-end Developer with over nine years extensive experience. </br></br>Working to create immersive User Interfaces I am well versed in using industry standard design software alongside up and coming Front-end technologies.</br></br>With a focus on developing responsive accessible sites I enjoy a hands on approach; from initial concepts and prototyping through to the development stage",
+        introduction: "An experienced Lead Product Designer and Front-end Developer with 10+ years of extensive experience. </br></br>Working to create immersive User Interfaces I am well versed in using industry standard design software alongside up and coming Front-end technologies.</br></br>With a focus on developing responsive accessible sites I enjoy a hands on approach; from initial concepts and prototyping through to the development stage",
         contactNumber: "07759 952035",
         contactEmail: "matthewdm@live.co.uk",
         contactAddress: "East Morton",
